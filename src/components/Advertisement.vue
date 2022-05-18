a78d75e4e962e85a7183d00ff5487df57111a568
Owall <97934878+MayOwall@users.noreply.github.com>
2022-05-18 15:30:18 +0900

    part1

---
+++ src/components/Advertisement.vue
@@ -0,0 +1,30 @@
+<template lang="">
+  <div class="Advertisement">지금 구매하면 {{ percentage }}% 할인</div>
+</template>
+<script>
+export default {
+  name: "Advertisement",
+  data() {
+    return {
+      percentage: 30,
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.percentage--;
+    }, 1000);
+  },
+};
+</script>
+<style lang="scss">
+.Advertisement {
+  width: 100%;
+  height: 5rem;
+  color: darkslateblue;
+  background-color: lightblue;
+  margin-top: 1rem;
+  border-radius: 0.5rem;
+  text-align: center;
+  line-height: 5rem;
+}
+</style>
---
+++ src/components/RoomCard.vue
@@ -0,0 +1,32 @@
+<template lang="">
+  <div class="RoomCard">
+    <div @click="$emit('roomInfoClick')" class="roomInfo">
+      <img :src="room.image" alt="방 이미지" />
+      <h4>{{ room.title }}</h4>
+      <p>{{ room.price }}</p>
+    </div>
+    <div class="roomReport" @click="$emit('reportButtonClick', room.id)">
+      <button>허위매물 신고</button>
+      <span>신고 수 : {{ room.reportNum }}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "RoomCard",
+  props: {
+    room: Object,
+  },
+};
+</script>
+<style lang="scss">
+.RoomCard {
+  padding: 1rem 0;
+
+  .roomInfo {
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
